d6ef23be85de3ead0d6a43e4d1b276eaaf0174eb
lucastupai <luc.puentes@duocuc.cl>
2026-05-04 21:00:33 -0400

    BFFcontroller
    se crea codigo controller

--- src/main/java/cl/sanosysalvo/BFF/controller/BFFcontroller.java
+++ src/main/java/cl/sanosysalvo/BFF/controller/BFFcontroller.java
@@ -1,5 +1,22 @@
 package cl.sanosysalvo.BFF.controller;
 
+import cl.sanosysalvo.BFF.dto.MascotadetalleDTO;
+import cl.sanosysalvo.BFF.service.BFFservice;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bff")
+@CrossOrigin("*")
 public class BFFcontroller {
-    
+
+    private final BFFservice bffService;
+
+    public BFFcontroller(BFFservice bffService) {
+        this.bffService = bffService;
+    }
+
+    @GetMapping("/mascotas/{id}/detalle")
+    public MascotadetalleDTO obtenerDetalleMascota(@PathVariable Long id) {
+        return bffService.obtenerDetalleMascota(id);
+    }
 }
